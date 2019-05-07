d3a4bd162db35c4bd7a95c4c1a573d037d577dca
KC Beard <konnor.beard@gmail.com>
2019-05-07 15:31:57 -0400

    helpful additions/scaffolding

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@
 
 mkdir -p ~/.config
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 echo "Removing old files"
 # remove everything, link to our directory
 rm ~/.bash_profile ~/.bashrc ~/.gitconfig ~/.gitignore_global ~/.inputrc ~/.zshrc
@@ -14,31 +16,35 @@ ln -s ~/dotfiles/gitignore_global   ~/.gitignore_global
 ln -s ~/dotfiles/inputrc            ~/.inputrc
 ln -s ~/dotfiles/zsh/zshrc          ~/.zshrc
 
+complete_setup() {
+    echo "Completing setup"
+
+    # install nvm
+    # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+    # install brew
+    # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    # brew update    
+
+    # install brewfile dependencies
+    # brew bundle install
+
+    # Run this after the rest of the script
+
+    # pyenv install 2.7.15
+    # pyenv install 3.6.7
+    # pyenv install 3.7.3
+    # pyenv virtualenv 3.7.3 neovim
+    # pyenv global 3.7.3
+
+    # PYENV_VERSION=neovim pip install pip --upgrade
+    # PYENV_VERSION=3.6.7 pip install pip --upgrade
+    # PYENV_VERSION=3.7.3 pip install pip --upgrade
+
+    # PYENV_VERSION=neovim pip install neovim --upgrade
+    # PYENV_VERSION=3.6.7 pip install ipython
+    # PYENV_VERSION=3.7.3 pip install ipython
+}
+
 echo "Done. Open a new terminal window to update theme."
-
-# install brew
-# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# brew update
-
-# install brewfile dependencies
-# brew bundle install
-
-# install vim-plug
-# curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Run this after the rest of the script
-
-# pyenv install 2.7.15
-# pyenv install 3.6.7
-# pyenv install 3.7.3
-# pyenv virtualenv 3.7.3 neovim
-# pyenv global 3.7.3
-
-# PYENV_VERSION=neovim pip install pip --upgrade
-# PYENV_VERSION=3.6.7 pip install pip --upgrade
-# PYENV_VERSION=3.7.3 pip install pip --upgrade
-
-# PYENV_VERSION=neovim pip install neovim --upgrade
-# PYENV_VERSION=3.6.7 pip install ipython
-# PYENV_VERSION=3.7.3 pip install ipython
